8e2c631b8f4d5d5deff9d098b50d9d7edc0f893f
themetropolitanchurch <themetropolitanchurch781@gmail.com>
2026-05-16 23:50:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -658,7 +658,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-            navigator.serviceWorker.register('service-worker.js')
+            navigator.serviceWorker.register('./service-worker.js')
                 .then((registration) => console.log('Service worker registered.', registration))
                 .catch((error) => console.warn('Service worker registration failed:', error));
         });
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tmc-cache-v2';
+const CACHE_NAME = 'tmc-cache-v3';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
